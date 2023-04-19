7ca80c0c1e74d8fe614e6bf66a13004235f7cbdd
andrew-gagliostro <argagliostro@gmail.com>
2023-04-19 19:51:10 -0400

    updates some eslint

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import UserUpdateForm from "../../ui-components/UserUpdateForm";
+import React, { Component } from "react";
+import HomeLinks from "@/components/layout/HomeLinks";
+import { Hero } from "@/components/hero/Hero";
+import { Typography } from "@material-ui/core";
+import { NavbarTwoColumns } from "@/components/navigation/NavbarTwoColumns";
+import { DropDown } from "@/components/navigation/DropDown";
+import { Logo } from "@/components/hero/Logo";
+import { NavBar } from "@/components/navigation/NavBar";
+import { Footer } from "@/components/layout/Footer";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col justify-between items-center">
+      <Hero />
+      <UserUpdateForm />
+      <Footer></Footer>
+    </main>
+  );
+}
--- src/ui-components/FollowUpdateForm.d.ts
+++ src/ui-components/FollowUpdateForm.d.ts
@@ -5,40 +5,59 @@
  **************************************************************************/
 
 import * as React from "react";
-import { GridProps, SelectFieldProps, TextFieldProps } from "@aws-amplify/ui-react";
+import {
+  GridProps,
+  SelectFieldProps,
+  TextFieldProps,
+} from "@aws-amplify/ui-react";
 import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
 import { Follow } from "../models";
 export declare type ValidationResponse = {
-    hasError: boolean;
-    errorMessage?: string;
+  hasError: boolean;
+  errorMessage?: string;
 };
-export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type ValidationFunction<T> = (
+  value: T,
+  validationResponse: ValidationResponse
+) => ValidationResponse | Promise<ValidationResponse>;
 export declare type FollowUpdateFormInputValues = {
-    following_user_id?: number;
-    followed_user_id?: number;
-    status?: string;
+  following_user_id?: number;
+  followed_user_id?: number;
+  status?: string;
 };
 export declare type FollowUpdateFormValidationValues = {
-    following_user_id?: ValidationFunction<number>;
-    followed_user_id?: ValidationFunction<number>;
-    status?: ValidationFunction<string>;
+  following_user_id?: ValidationFunction<number>;
+  followed_user_id?: ValidationFunction<number>;
+  status?: ValidationFunction<string>;
 };
-export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type PrimitiveOverrideProps<T> = Partial<T> &
+  React.DOMAttributes<HTMLDivElement>;
 export declare type FollowUpdateFormOverridesProps = {
-    FollowUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
-    following_user_id?: PrimitiveOverrideProps<TextFieldProps>;
-    followed_user_id?: PrimitiveOverrideProps<TextFieldProps>;
-    status?: PrimitiveOverrideProps<SelectFieldProps>;
+  FollowUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
+  following_user_id?: PrimitiveOverrideProps<TextFieldProps>;
+  followed_user_id?: PrimitiveOverrideProps<TextFieldProps>;
+  status?: PrimitiveOverrideProps<SelectFieldProps>;
 } & EscapeHatchProps;
-export declare type FollowUpdateFormProps = React.PropsWithChildren<{
+export declare type FollowUpdateFormProps = React.PropsWithChildren<
+  {
     overrides?: FollowUpdateFormOverridesProps | undefined | null;
-} & {
+  } & {
     id?: string;
     follow?: Follow;
-    onSubmit?: (fields: FollowUpdateFormInputValues) => FollowUpdateFormInputValues;
+    onSubmit?: (
+      fields: FollowUpdateFormInputValues
+    ) => FollowUpdateFormInputValues;
     onSuccess?: (fields: FollowUpdateFormInputValues) => void;
-    onError?: (fields: FollowUpdateFormInputValues, errorMessage: string) => void;
-    onChange?: (fields: FollowUpdateFormInputValues) => FollowUpdateFormInputValues;
+    onError?: (
+      fields: FollowUpdateFormInputValues,
+      errorMessage: string
+    ) => void;
+    onChange?: (
+      fields: FollowUpdateFormInputValues
+    ) => FollowUpdateFormInputValues;
     onValidate?: FollowUpdateFormValidationValues;
-} & React.CSSProperties>;
-export default function FollowUpdateForm(props: FollowUpdateFormProps): React.ReactElement;
+  } & React.CSSProperties
+>;
+export default function FollowUpdateForm(
+  props: FollowUpdateFormProps
+): React.ReactElement;
